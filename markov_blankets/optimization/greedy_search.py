ebaf4ec0b4b81d4f7df87bfa7f97dc8cfae5b646
Sergio Kef <sergiokef@gmail.com>
2020-06-17 00:34:47 +0200

    Refactor greedy_search sorting

--- markov_blankets/optimization/greedy_search.py
+++ markov_blankets/optimization/greedy_search.py
@@ -6,16 +6,17 @@ Created on Mon May 25 19:45:52 2020
 @author: Panagiotis Mandros
 """
 
-import numpy as np 
-import pandas as pd
+import time
 from operator import itemgetter
+
+import numpy as np
+import pandas as pd
+
 from information_theory.estimators import mutual_information_permutation_upper_bound
 from information_theory.information_theory_basic import mutual_information_plugin
 
 
-import time
-
-def greedy_search(estimator,data,target=None,limit=None,prior_solution_set=None):
+def greedy_search(estimator, data, target=None, limit=None, prior_solution_set=None):
     """
     Given data, it greedily maximizes an estimator of a dependency measure D(XY), over
     candidate attribute sets in X in the data. It selects the best candidate to expand 
@@ -24,106 +25,98 @@ def greedy_search(estimator,data,target=None,limit=None,prior_solution_set=None)
     wont continue after the second level (pairs of attributes). A prior_solution_set can be used to
     initialize the search.    
     """
-    if isinstance(data, pd.DataFrame):  
-        data=data.to_numpy();
-        
-    number_explanatory_variables=np.size(data,1)-1
+    if isinstance(data, pd.DataFrame):
+        data = data.to_numpy()
 
-    if target==None:
-        target_index=number_explanatory_variables;
+    number_explanatory_variables = np.size(data, 1) - 1
+
+    if target is None:
+        target_index = number_explanatory_variables
     else:
-        target_index=target-1;
-    Y=data[:,target_index]; 
+        target_index = target - 1
+    Y = data[:, target_index]
 
-    if limit==None:
-        limit=number_explanatory_variables 
-    
-    setOfCandidates=set([i for i in range(number_explanatory_variables+1)]) 
-    setOfCandidates.remove(target_index) 
+    if limit is None:
+        limit = number_explanatory_variables
 
-    if prior_solution_set==True:
-        setOfCandidates.difference_update(prior_solution_set) 
-        
-    
-    selectedVariables=set() 
-    bestScore=-1000 
-    
+    set_of_candidates = set(range(number_explanatory_variables + 1))
+    set_of_candidates.remove(target_index)
+
+    if prior_solution_set:
+        set_of_candidates.difference_update(prior_solution_set)
+
+    selected_variables = set()
+    best_score = -1000
+
     for i in range(limit):
 
-        candidateScores=[evaluateCandidate(estimator,data,index,Y,selectedVariables) for index in setOfCandidates]    
-                                   
-        candidateScores.sort(key=itemgetter(0),reverse=True) 
-        
-        top=candidateScores[0] 
-        topScore=top[0] 
-        topCandidateIndex=top[1] 
+        generator = sort_generator(
+            estimator=estimator,
+            data=data,
+            Y=Y,
+            selected=selected_variables,
+            candidates=set_of_candidates,
+        )
 
-        if topScore<=0 or topScore<=bestScore:
-            return [selectedVariables,bestScore] 
-       
-        if topScore>bestScore:
-            bestScore=topScore 
-            selectedVariables.add(topCandidateIndex) 
-            setOfCandidates.remove(topCandidateIndex) 
-    return [selectedVariables,bestScore] 
-        
-    
-def evaluateCandidate(estimator,data,candidateIndex,Y,alreadySelectedVariables):
-    jointIndices=set(alreadySelectedVariables) 
-    jointIndices.add(candidateIndex) 
-    result=estimator(data[:,list(jointIndices)],Y),candidateIndex 
+        top_score, top_candidate_index = max(generator, key=itemgetter(0))
 
-    return result
-    
-    
+        if top_score <= 0 or top_score <= best_score:
+            return selected_variables, best_score
+
+        if top_score > best_score:
+            best_score = top_score
+            selected_variables.add(top_candidate_index)
+            set_of_candidates.remove(top_candidate_index)
+    return selected_variables, best_score
+
+
+def sort_generator(estimator, data, Y, selected, candidates):
+    for candidate in candidates:
+        val = estimator(data[:, (*selected, candidate)], Y)
+        yield val, candidate
+
+
 def main():
-    data = pd.read_csv("../datasets/tic_tac_toe.csv");
+    data = pd.read_csv("../datasets/tic_tac_toe.csv")
     biggerData=data.append(data).append(data).append(data).append(data).append(data).append(data)
     biggerBiggerData=biggerData.append(biggerData).append(biggerData).append(biggerData).append(biggerData).append(biggerData)
     biggerBiggerData=biggerBiggerData.append(biggerBiggerData).append(biggerBiggerData).append(biggerBiggerData).append(biggerBiggerData)
-      
+
     # print(data.info())
     # print(biggerBiggerData.info())
-    
+
     start_time=time.time()
     [selected,bestScore]=greedy_search(mutual_information_plugin,data)
     print("--- %s seconds for greedy search in small data with plugin MI ---" % (time.time() - start_time))
     print(f' selected by plugin MI on small: {selected} with score {bestScore}')
-    
+
     start_time=time.time()
     [selected,bestScore]=greedy_search(mutual_information_permutation_upper_bound,data)
     print("--- %s seconds for greedy search in small data with upper-bound MI ---" % (time.time() - start_time))
     print(f' selected by upper-bound MI on small: {selected} with score {bestScore}')
     assert(0.28707781833145635==bestScore)
 
-
     start_time=time.time()
     [selected,bestScore]=greedy_search(mutual_information_plugin,data)
     print("--- %s seconds for greedy search in small data with plugin FI ---" % (time.time() - start_time))
     print(f' selected by plugin FI on small: {selected} with score {bestScore}')
-    
+
     start_time=time.time()
     [selected,bestScore]=greedy_search(mutual_information_permutation_upper_bound,data)
     print("--- %s seconds for greedy search in small data with upper-bound FI ---" % (time.time() - start_time))
     assert(0.3083695032958582==bestScore)
     print(f' selected by upper-bound FI on small: {selected} with score {bestScore}')
-    
-    
-    
-    
+
     # start_time=time.time()
     # [selected,bestScore]=greedy_search(mutual_information_plugin,biggerBiggerData)
     # print("--- %s seconds for greedy search in big data with plugin FI ---" % (time.time() - start_time))
     # print(f' selected by plugin FI on big: {selected}')
-    
+
     # start_time=time.time()
     # [selected,bestScore]=greedy_search(mutual_information_permutation_upper_bound,biggerBiggerData)
     # print("--- %s seconds for greedy search in big data with upper-bound FI ---" % (time.time() - start_time))
     # print(f' selected by upper-bound FI on big: {selected}')
 
-    
 
-
-
-if __name__ == '__main__':
-    main()   
+if __name__ == "__main__":
+    main()
