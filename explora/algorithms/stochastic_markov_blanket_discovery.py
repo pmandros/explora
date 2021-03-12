5a6d65ea7dec48f5ac10b1bd58bb0957204dbf90
Panagiotis Mandros <mandros.p@gmail.com>
2021-03-12 13:22:01 +0100

    added stochastic markov blanket discovery

---
+++ explora/algorithms/stochastic_markov_blanket_discovery.py
@@ -0,0 +1,12 @@
+from explora.optimization import greedy_search
+
+
+def stochastic_markov_blanket_discovery(estimator, data, target_variable=None, limit=None, control_variables=None):
+    """
+    Markov blanket discovery with stochastic greedy
+    """
+    selected, best_score = greedy_search(estimator, data, target_variable=target_variable, limit=limit,
+                                         is_stochastic=True,
+                                         control_variables=control_variables)
+
+    return selected, best_score
